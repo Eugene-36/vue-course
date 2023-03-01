19342dab5ef4330eeeebc5c00018e61d1b5e79e5
Eugene-36 <livenskiyjenya@gmail.com>
2023-03-01 22:00:53 +0200

    added base

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 class="title" @click="likes += 1">click likes -{{ likes }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+    };
+  },
+};
+</script>
+
+<style>
+.title {
+  color: orange;
+}
+</style>
